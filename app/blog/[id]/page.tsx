77516901f7490c7d0ae0e13dbbd4c2f12839ecb4
Afanasi <narik.fara@mail.ru>
2025-03-30 16:36:55 +0300

    first commit of this project

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,37 @@
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  return response.json();
+}
+
+export async function generateMetadata({
+  params: { id },
+}: Props) {
+  const post = await getData(id);
+
+  return {
+    title: post.title,
+  };
+};
+
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  return (
+    <>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  if (!response.ok) throw new Error("Unable to fetch posts");
+  return response.json();
+}
+
+export const metadata: Metadata = {
+  title: "Blog | Next App",
+};
+
+export default async function Blog() {
+  const posts = await getData();
+
+  return (
+    <>
+      <h2>Blog page</h2>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
