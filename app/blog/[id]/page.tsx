82e917a1f90cba59af008367241b98b1cb5aa11c
Afanasi <narik.fara@mail.ru>
2025-03-31 03:27:14 +0300

    new branch saved

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,7 +3,7 @@ type Props = {
     id: string;
   };
 };
-
+// simple nothing
 async function getData(id: string) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}`,
